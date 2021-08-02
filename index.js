776b661d6f7a661bd8bfef3c6ec24fdf509192f4
Adam Gonzales <adamgonzales1@gmail.com>
2021-08-02 12:40:17 -0700

    feature: add coinGecko api

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-console.log('working')
-
 const pageContainer = document.getElementById('page-container')
+const unSplashAPI = 'https://apis.scrimba.com/unsplash/photos/'
+const coinGeckoAPI = 'https://api.coingecko.com/api/v3/'
 
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+fetch(`${unSplashAPI}random?orientation=landscape&query=nature`)
   .then(res => res.json())
   .then(data => {
-    console.log(data)
+    // console.log(data)
     pageContainer.style.backgroundImage = `url(${data.urls.full})`
     document.getElementById('image-author').textContent = `📸: ${data.user.first_name} ${data.user.last_name ? ' ' + data.user.last_name : ''}`
   })
@@ -13,3 +13,10 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     console.error(err)
     pageContainer.style.backgroundImage = 'url(https://images.unsplash.com/reserve/HgZuGu3gSD6db21T3lxm_San%20Zenone.jpg?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Mjc4NzU2OTI&ixlib=rb-1.2.1&q=85)'
   })
+
+fetch(`${coinGeckoAPI}coins/dogecoin`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.market_data.current_price.usd)
+  })
+  .catch(err => console.error(err))
