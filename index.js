345c512b1acaa282b1ec348ece6f686bf7bb4627
Adam Gonzales <adamgonzales1@gmail.com>
2021-08-03 11:18:44 -0700

    feature: finish coingecko information display, add displayCurrentTime()

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const pageContainer = document.getElementById('page-container')
+const cryptoInfoContainer = document.getElementById('crypto-info__container')
+const timeDisplay = document.getElementById('time__display')
 const unSplashAPI = 'https://apis.scrimba.com/unsplash/photos/'
 const coinGeckoAPI = 'https://api.coingecko.com/api/v3/'
 
@@ -15,8 +17,31 @@ fetch(`${unSplashAPI}random?orientation=landscape&query=nature`)
   })
 
 fetch(`${coinGeckoAPI}coins/dogecoin`)
-  .then(res => res.json())
+  .then(res => {
+    if (!res.ok) {
+      cryptoInfo.textContent = 'Data not available at this time'
+      throw Error('Something went wrong')
+    }
+    return res.json()
+  })
   .then(data => {
-    console.log(data.market_data.current_price.usd)
+    cryptoInfoContainer.innerHTML = `
+    <div id="crypto-info__details" class="crypto-info__row">
+      <img class="crypto-info__image" src="${data.image.small}" alt="${data.name}" />
+      <span>${data.name}</span>
+    </div>
+    <ul class="no-bullets">
+      <li>🤑: $${data.market_data.current_price.usd}</li>
+      <li>⬆️: $${data.market_data.high_24h.usd}</li>
+      <li>⬇️: $${data.market_data.low_24h.usd}</li>
+    </ul>
+    `
   })
   .catch(err => console.error(err))
+
+function displayCurrentTime () {
+  const currentTime = new Date().toLocaleTimeString([], { timeStyle: 'short' })
+  timeDisplay.textContent = currentTime
+}
+
+setInterval(displayCurrentTime)
